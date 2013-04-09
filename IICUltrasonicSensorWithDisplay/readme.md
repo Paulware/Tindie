85e821a65f49f44aab5ac554b1d22e4efae1ce71
Paul Richards <paulware@hotmail.com>
2013-04-08 22:42:06 -0500

    v0.03
    Added NunChuk

--- IICUltrasonicSensorWithDisplay/readme.md
+++ IICUltrasonicSensorWithDisplay/readme.md
@@ -2,7 +2,7 @@ This Ultrasonic sensor has an IIC (I Squared C) interface that allows you to
 easily add it to any project
 
 The example master sketch to read the value in cm is located:
-  cm.ino
+  cm/cm.ino
   
 It requires the Wire library files: 
   Wire.h and Wire.cpp
---
+++ Nunchuk/Nunchuk.ino
@@ -0,0 +1,56 @@
+/*
+ * ArduinoNunchukDemo.ino
+ *
+ * Copyright 2011-2013 Gabriel Bianconi, http://www.gabrielbianconi.com/
+ *
+ * Project URL: http://www.gabrielbianconi.com/projects/arduinonunchuk/
+ *
+ */
+
+#include <Wire.h>
+#include <ArduinoNunchuk.h>
+
+#define BAUDRATE 19200
+
+ArduinoNunchuk nunchuk = ArduinoNunchuk();
+
+void setup()
+{
+  Serial.begin(BAUDRATE);
+  nunchuk.init();
+  for (int i=0; i<4; i++)
+  {
+    pinMode (i+4, OUTPUT);
+    digitalWrite(i+4,1);
+  }
+}
+
+void loop()
+{
+  nunchuk.update();
+
+  Serial.print(nunchuk.analogX, DEC);
+  Serial.print(' ');
+  Serial.print(nunchuk.analogY, DEC);
+  Serial.print(' ');
+  Serial.print(nunchuk.accelX, DEC);
+  Serial.print(' ');
+  Serial.print(nunchuk.accelY, DEC);
+  Serial.print(' ');
+  Serial.print(nunchuk.accelZ, DEC);
+  Serial.print(' ');
+  Serial.print(nunchuk.zButton, DEC);
+  if (nunchuk.zButton)
+    digitalWrite (4,0);
+  else
+    digitalWrite (4,1);
+    
+  if (nunchuk.cButton)
+    digitalWrite ( 6,0);
+  else
+    digitalWrite ( 6,1); 
+    
+  
+  Serial.print(' ');
+  Serial.println(nunchuk.cButton, DEC);
+}
