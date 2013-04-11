38d87500059c37d67ba2369b37b9296836b00319
Paul Richards <paulware@hotmail.com>
2013-04-10 21:15:13 -0500

    v0.04
    Added LCDScreen example

---
+++ LCDScreen/LCDScreen.ino
@@ -0,0 +1,65 @@
+// we need this library for the LCD commands
+#include <LiquidCrystal.h> 
+#include <SPI.h>
+
+#define COLUMNS 20
+#define ROWS 4
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
+int state = 0;
+int x = COLUMNS - 1;
+int y = 0;
+void setup()
+{
+  Serial.begin (115200);
+  lcd.begin(COLUMNS, ROWS); // Specify how many columns and rows in the LCD unit
+  lcd.clear();
+  lcd.print(" HD44780 LCD Ready  ");
+  lcd.setCursor (0,1); // necessary
+  lcd.print("All digits check out");
+  lcd.setCursor (0,2); // necessary
+  lcd.print("That is all...Have a");
+  lcd.setCursor (0,3); // necessary
+  lcd.print("Great Day! Adios bye");
+} 
+
+void handleCh()
+{
+  char ch = 0;
+  if (Serial.available())
+    ch = Serial.read();
+  
+  // Ignore carriage return/line feed
+  if ((ch == 10) || (ch == 13)) 
+    ch = 0;
+  
+  if (ch)
+  {
+    x = x + 1;
+    if (x == COLUMNS) 
+    {
+      x = 0;
+      y = y + 1;
+      if (y == ROWS) 
+        y = 0;
+    } 
+    
+    if (ch == '^') // clearscreen
+    {
+      lcd.clear();
+      x = COLUMNS -1;
+      y = ROWS - 1;
+    }
+    else
+    {
+      lcd.setCursor (x,y);
+      lcd.print ( ch );   
+    }  
+  }    
+}
+
+void loop()
+{
+  handleCh ();
+}
